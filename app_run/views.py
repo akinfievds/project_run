b663988622cf3117adc979d90dbea5c0f84d14d8
Akinfiev Dmitrii <akinfievds@gmail.com>
2025-10-16 09:57:02 +0300

    4.1 Task Fix

--- app_run/views.py
+++ app_run/views.py
@@ -1,16 +1,20 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework import viewsets
 
 from app_run.models import Run
 from app_run.serializers import RunSerializer
+from django.conf import settings
 
 
+@api_view(['GET'])
 def company_details_view(request):
-    return JsonResponse({
-        'company_name': 'Runners for the values',
-        'slogan':'Anywhere, anytime, anywhat...',
-        'contacts':'city-district-country'
-    })
+    details = {
+        'company_name': settings.COMPANY_NAME,
+        'slogan': settings.SLOGAN,
+        'contacts': settings.CONTACTS
+    }
+    return Response(details)
 
 
 class RunViewSet(viewsets.ModelViewSet):
