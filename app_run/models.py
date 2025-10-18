d8801325aa33858aa4f0b7122ac033135483541c
Akinfiev Dmitrii <akinfievds@gmail.com>
2025-10-19 00:03:16 +0300

    9.1 Run Status. Add status field to model

--- app_run/models.py
+++ app_run/models.py
@@ -5,6 +5,13 @@ user = get_user_model()
 
 
 class Run(models.Model):
+    STATUSES = [
+        ('init', 'INIT'),
+        ('in_progress', 'IN_PROGRESS'),
+        ('finished', 'FINISHED')
+    ]
+
     created_at = models.DateTimeField(auto_now_add=True)
     athlete = models.ForeignKey(user, on_delete=models.CASCADE)
     comment = models.CharField(max_length=255)
+    status = models.CharField(max_length=12, choices=STATUSES)
