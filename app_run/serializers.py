80a684e0a23745b71d78166fdc37cd81851b8434
Akinfiev Dmitrii <akinfievds@gmail.com>
2025-10-29 00:39:19 +0300

    'app_run.serializers' formatted

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404
 
 from app_run.models import AthleteInfo, Challenge, Run, Position
 
@@ -8,7 +7,7 @@ from app_run.models import AthleteInfo, Challenge, Run, Position
 class UserContractedSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'last_name', 'first_name',)
+        fields = ("id", "username", "last_name", "first_name", )
 
 
 class RunSerializer(serializers.ModelSerializer):
@@ -16,7 +15,7 @@ class RunSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Run
-        fields = '__all__'
+        fields = "__all__"
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -25,13 +24,14 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'date_joined', 'username', 'last_name', 'first_name', 'type', 'runs_finished')
+        fields = ("id", "date_joined", "username", "last_name",
+                  "first_name", "type", "runs_finished", )
 
     def get_type(self, obj):
-        return 'coach' if obj.is_staff else 'athlete'
+        return "coach" if obj.is_staff else "athlete"
 
     def get_runs_finished(self, obj):
-        return obj.runs.filter(status='finished').count()
+        return obj.runs.filter(status="finished").count()
 
 
 class AthleteInfoSerializer(serializers.ModelSerializer):
@@ -39,27 +39,27 @@ class AthleteInfoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = AthleteInfo
-        exclude = ('id', 'user',)
+        exclude = ("id", "user",)
 
     def get_user_id(self, obj):
         return obj.user.id
 
     def validate_weight(self, value):
         if value <= 0 or value >= 900:
-            raise serializers.ValidationError('Weight has to be between 1 and 10.')
+            raise serializers.ValidationError("Weight has to be between 1 and 10.")
         return value
 
 
 class ChallengeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Challenge
-        fields = '__all__'
+        fields = "__all__"
 
 
 class PositionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Position
-        fields = '__all__'
+        fields = "__all__"
 
     def validate_run(self, run):
         if not run.status == 'in_progress':
@@ -68,10 +68,10 @@ class PositionSerializer(serializers.ModelSerializer):
 
     def validate_latitude(self, latitude):
         if not -90.0 <= latitude <= 90.0:
-            raise serializers.ValidationError('Latitude has to be between -90.0 and 90.0.')
+            raise serializers.ValidationError("Latitude has to be between -90.0 and 90.0.")
         return latitude
 
     def validate_longitude(self, latitude):
         if not -180.0 <= latitude <= 180.0:
-            raise serializers.ValidationError('Longitude has to be between -180.0 and 180.0.')
+            raise serializers.ValidationError("Longitude has to be between -180.0 and 180.0.")
         return latitude
